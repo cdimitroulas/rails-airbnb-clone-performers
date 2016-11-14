0e4c79ebca37245d1c12e64eedfe5d973b8e2b19
Christos Dimitroulas <c.dimitroulas@gmail.com>
2016-11-14 17:12:27 +0000

    fixed some bug which occurred after adding :confirmable to the User model

--- db/schema.rb
+++ db/schema.rb
@@ -33,6 +33,7 @@ ActiveRecord::Schema.define(version: 20161114161117) do
     t.string   "confirmation_token"
     t.datetime "confirmed_at"
     t.datetime "confirmation_sent_at"
+    t.string   "unconfirmed_email"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
