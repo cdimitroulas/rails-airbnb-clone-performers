d22db5b8371fa1ddaf099d32ea4933393e494a8f
Will Tinney <willtinney1@gmail.com>
2016-11-15 18:17:21 +0000

    Continued back-end code in show page - still a bit left to do but more or less finished. Still have the booking component to add into the page.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161114175912) do
+ActiveRecord::Schema.define(version: 20161115164457) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -45,9 +45,24 @@ ActiveRecord::Schema.define(version: 20161114175912) do
     t.string   "video"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.integer  "members"
+    t.string   "requirements"
+    t.integer  "discount"
+    t.string   "cancellation_policy"
     t.index ["confirmation_token"], name: "index_performers_on_confirmation_token", unique: true, using: :btree
     t.index ["email"], name: "index_performers_on_email", unique: true, using: :btree
     t.index ["reset_password_token"], name: "index_performers_on_reset_password_token", unique: true, using: :btree
+  end
+
+  create_table "reviews", force: :cascade do |t|
+    t.string   "title"
+    t.text     "description"
+    t.integer  "user_id"
+    t.integer  "performer_id"
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
+    t.index ["performer_id"], name: "index_reviews_on_performer_id", using: :btree
+    t.index ["user_id"], name: "index_reviews_on_user_id", using: :btree
   end
 
   create_table "users", force: :cascade do |t|
@@ -75,4 +90,6 @@ ActiveRecord::Schema.define(version: 20161114175912) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
 
+  add_foreign_key "reviews", "performers"
+  add_foreign_key "reviews", "users"
 end
