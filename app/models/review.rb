d9040929f0591fab1ca0c49377e9aefd0cda87c3
Will Tinney <willtinney1@gmail.com>
2016-11-15 16:35:46 +0000

    Corrected model with rating

--- app/models/review.rb
+++ app/models/review.rb
@@ -2,6 +2,7 @@ class Review < ApplicationRecord
   belongs_to :user
   belongs_to :performer
 
+  validates :rating, presence: true, inclusion: { in: 0..5 }
   validates :title, presence: true
   validates :description, presence: true
 end
--- db/migrate/20161115161808_create_reviews.rb
+++ db/migrate/20161115161808_create_reviews.rb
@@ -1,6 +1,7 @@
 class CreateReviews < ActiveRecord::Migration[5.0]
   def change
     create_table :reviews do |t|
+      t.integer :rating
       t.string :title
       t.text :description
       t.references :user, foreign_key: true
