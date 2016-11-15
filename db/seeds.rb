279f0d21985b792dbea2e80e50aa1704e0d01238
Christos Dimitroulas <c.dimitroulas@gmail.com>
2016-11-15 11:38:21 +0000

    added faker gem and seeded some fake performers into the database

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,14 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+pictures = ["http://res.cloudinary.com/dplnt2ozo/image/upload/v1479209534/Airbnb-clone/AliciaKeys.jpg",
+            "http://res.cloudinary.com/dplnt2ozo/image/upload/v1479209538/Airbnb-clone/Snoop.jpg",
+            "http://res.cloudinary.com/dplnt2ozo/image/upload/v1479209531/Airbnb-clone/Rihana.jpg",
+            "https://cloudinary.com/console/media_library#/dialog/image/upload/Airbnb-clone/JustinB",
+            "http://res.cloudinary.com/dplnt2ozo/image/upload/v1479209526/Airbnb-clone/U2.jpg",
+            "http://res.cloudinary.com/dplnt2ozo/image/upload/v1479208813/Airbnb-clone/gettyimages_117087793_wide-4f9cc3824e22a75519017d35ddf5fb2e54e672f1-s900-c85.jpg"]
+categories = ["Rock", "Pop", "DJ", "Entertainers"]
+
+20.times do
+  Performer.create(email: Faker::Internet.email, password: Devise.friendly_token[0,20],
+                 first_name: Faker::Name.first_name, last_name: Faker::Name.last_name,
+                 phone_number: Faker::PhoneNumber.cell_phone , city: Faker::Address.city,
+                 picture: pictures.sample, category: categories.sample)
+end
