c530111a5fb41c6beaf6fd20a474db1aa701ab24
Christos Dimitroulas <c.dimitroulas@gmail.com>
2016-11-17 18:40:06 +0000

    refactored performers controller. search_filter methods created in the model. changed the event_types filtering to work with arrays

--- app/models/performer.rb
+++ app/models/performer.rb
@@ -25,4 +25,34 @@ class Performer < ApplicationRecord
 
   mount_uploader :profile_picture, PhotoUploader
   mount_uploader :picture, BannerPhotoUploader
+
+  def self.search_filter(params)
+    results = with_location.within_fifty(params[:postcode])
+    results = results.with_category(params[:category])
+    results = results.with_event_types(params[:event_types])
+
+    results
+  end
+
+  def self.with_location
+    where.not(latitude: nil, longitude: nil)
+  end
+
+  def self.within_fifty(postcode)
+    postcode.present? ? near(postcode, 50) : all
+  end
+
+  def self.with_category(category)
+    category.present? ? where(category: category) : all
+  end
+
+  def self.with_event_types(event_types_search)
+    if event_types_search.present?
+      all.map do |performer|
+        return performer if performer.event_types.include?(event_types_search)
+      end
+    else
+      all
+    end
+  end
 end
