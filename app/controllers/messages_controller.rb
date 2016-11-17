23b79109c42a8fa66b5cb95b1b55035c011eb729
Christos Dimitroulas <c.dimitroulas@gmail.com>
2016-11-17 17:07:48 +0000

    Updated messagescontroller

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,15 +1,15 @@
 class MessagesController < ApplicationController
 
   def new
-    @message = Message.new(message_params)
+    @message = Message.new
   end
 
   def create
-    @message = current_user.bookings.build(message_params)
+    @message = current_user.bookings.messages.build(message_params)
     @message.performer_id = params[:performer_id]
 
     if @message.save
-      redirect_to @message, notice: 'Message was successfully created.'
+      redirect_to @message, notice: 'Message was successfully sent.'
     else
       render :new
     end
@@ -18,6 +18,7 @@ class MessagesController < ApplicationController
   def show
     @message = Message.find(params[:id])
     @message.read = true
+    @message.save
   end
 
   private
