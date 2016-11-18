4154f48ad879c8ee22f617b3f8d35aded6cb45b6
Will Tinney <willtinney1@gmail.com>
2016-11-18 12:45:10 +0000

    Created migration adding message column to bookings table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161117151157) do
+ActiveRecord::Schema.define(version: 20161118124148) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,6 +24,7 @@ ActiveRecord::Schema.define(version: 20161117151157) do
     t.integer  "performer_id"
     t.datetime "created_at",   null: false
     t.datetime "updated_at",   null: false
+    t.text     "message"
     t.index ["performer_id"], name: "index_bookings_on_performer_id", using: :btree
     t.index ["user_id"], name: "index_bookings_on_user_id", using: :btree
   end
